7873cf0938c8e209c2980cf01041f9bcf5618ec1
meflems <89581469+meflems@users.noreply.github.com>
2022-04-08 21:27:12 +0700

    update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ client = commands.Bot(
 
 @client.event
 async def on_connect():
-  await client.change_presence(activity = discord.Streaming(name = " INI DI UBAH ", url = "https://twicth.tv/"))
+  await client.change_presence(activity = discord.Streaming(name = " INI DI UBAH ", url = "https://frownmc.xyz"))
 
 
 keep_alive.keep_alive()
