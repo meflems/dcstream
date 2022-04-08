76cbe3d7dda57ae5636390ff533430a50b38ba4c
meflems <89581469+meflems@users.noreply.github.com>
2022-04-08 23:08:49 +0700

    update index.js

--- index.js
+++ index.js
@@ -3,23 +3,10 @@ const client = new Discord.Client();
  
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  const updatePresence = async (client, state) => {
-    // Set the presence
-    const activity = {
-        name: 'Frown Cloud',
-        type: 'WATCHING',
-        details: 'frownmc.xyz',
-        state: state,
-        timestamps: {
-            start: Date.now(),
-        },
-     };
-    client.user.setPresence({
-        pid: process.pid,
-        activity: activity,
-        status: 'online',
-    });
-  };
+  client.user.setActivity("become admin on Frown Cloud", {
+    type: "STREAMING",
+    url: "https://frownmc.xyz"
+  });
 });
  
 client.login(process.env.TOKEN);
